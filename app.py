3137688e14a962b629b2fbe7a201575860d6ecfb
Shubham Bhavsar <shubhamcbhavsar198@gmail.com>
2026-01-02 09:41:50 +0530

    Integrate AWS Secrets Manager for DB credentials
    Added AWS Secrets Manager integration to retrieve database credentials.

--- app.py
+++ app.py
@@ -3,11 +3,38 @@ import pymysql
 
 app = Flask(__name__)
 
+import boto3
+from botocore.exceptions import ClientError
+
+
+def get_secret():
+    secret_name = "rds!db-9ea6f069-b806-409c-adf0-6f4c051ede2c"
+    region_name = "ap-south-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+
+
 def get_db_connection():
-    connection = pymysql.connect(host='mydb.cylck8yh5jkc.eu-central-1.rds.amazonaws.com',  # Replace with your RDS endpoint
-                                 user='dbuser',      # Replace with your RDS username
-                                 password='dbpassword',  # Replace with your RDS password
-                                 db='devprojdb',   # Replace with your database name
+    connection = pymysql.connect(host=secret["host"],  # Replace with your RDS endpoint
+                                 user=secret["username"],      # Replace with your RDS username
+                                 password=secret["password"],  # Replace with your RDS password
+                                 db=secret["dbname"],   # Replace with your database name
                                  charset='utf8mb4',
                                  cursorclass=pymysql.cursors.DictCursor)
     return connection
