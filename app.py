d99d8bf72057256c1a2da22c8bfa3bfe796384f8
Shubham <Shubhamcbhavsar198@gmail.com>
2026-01-04 10:11:21 +0530

    removed secret

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    secret = get_secret()
+    
     app.run(debug=True, host='0.0.0.0')
 
     # print(secret)
